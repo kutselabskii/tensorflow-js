e978234d9bac511dfa07b403813ef6b2769c1857
Egor Kutselabskii <osutanfirst@gmail.com>
2020-12-24 21:23:52 +1000

    Add working tensorflow.js example, build all dependencies, add package.json

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const http = require('http')
+const tf = require('@tensorflow/tfjs');
 
-const hostname = '127.0.0.1'
-const port = 3000
+// Optional Load the binding:
+// Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+require('@tensorflow/tfjs-node');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!\n')
-})
+// Train a simple model:
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
+});
