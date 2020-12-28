6805947c3d113e1c7fd347e8b8b53bc1564625a1
Egor Kutselabskii <osutanfirst@gmail.com>
2020-12-28 18:56:20 +1000

    Add working predictions usage example

--- public/webcam.js
+++ public/webcam.js
@@ -1,16 +1,67 @@
-const button = document.getElementById('webcamButton');
-button.addEventListener('click', buttonClicked);
+const enableWebcamButton = document.getElementById('webcamButton');
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
 
-function buttonClicked() {
-    var video = document.getElementById('webcam');
-    window.navigator.mediaDevices.getUserMedia({ video: true })
-      .then(stream => {
-          video.srcObject = stream;
-          video.onloadedmetadata = (e) => {
-              video.play();
-          };
-      })
-      .catch( () => {
-          alert('You have give browser the permission to run Webcam and mic ;( ');
-      });
+var model = undefined;
+var children = [];
+
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  demosSection.classList.remove('invisible');
+});
+
+enableWebcamButton.addEventListener('click', buttonClicked);
+
+function buttonClicked(event) {
+  event.target.classList.add('removed');  
+
+  const constraints = {
+    video: true
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+  });
+}
+
+function predictWebcam() {
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+    
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + '% confidence.';
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+            + predictions[n].bbox[1] + 'px; width: ' 
+            + predictions[n].bbox[2] + 'px; height: '
+            + predictions[n].bbox[3] + 'px;';
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
 }
