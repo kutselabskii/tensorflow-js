41a62addf744d14a8912c45306115fedbb9f9f84
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-09 22:28:17 +1000

    Checked that pre-trained models is available

--- keraspy/main.py
+++ keraspy/main.py
@@ -1,26 +1,32 @@
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
-import tensorflowjs as tfjs
+# from numpy import loadtxt
+# from keras.models import Sequential
+# from keras.layers import Dense
+# import tensorflowjs as tfjs
 
-from pathlib import Path
+# from pathlib import Path
 
-datapath = str(Path(__file__).resolve().parent.joinpath('data.csv'))
-modelpath = str(Path(__file__).resolve().parent.parent.joinpath('public/model'))
+# datapath = str(Path(__file__).resolve().parent.joinpath('data.csv'))
+# modelpath = str(Path(__file__).resolve().parent.parent.joinpath('public/model'))
 
-dataset = loadtxt(datapath, delimiter=',')
-X = dataset[:, 0:8]
-y = dataset[:, 8]
+# dataset = loadtxt(datapath, delimiter=',')
+# X = dataset[:, 0:8]
+# y = dataset[:, 8]
 
-model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model = Sequential()
+# model.add(Dense(12, input_dim=8, activation='relu'))
+# model.add(Dense(8, activation='relu'))
+# model.add(Dense(1, activation='sigmoid'))
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X, y, epochs=30, batch_size=10)
+# model.fit(X, y, epochs=30, batch_size=10)
 
-_, accuracy = model.evaluate(X, y)
-print(f'Accuracy: {round(accuracy * 100, 2)}%')
+# _, accuracy = model.evaluate(X, y)
+# print(f'Accuracy: {round(accuracy * 100, 2)}%')
 
-tfjs.converters.save_keras_model(model, modelpath)
+# tfjs.converters.save_keras_model(model, modelpath)
+
+from keras.applications.vgg16 import VGG16
+# load model
+model = VGG16()
+# summarize the model
+model.summary()
