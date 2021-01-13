3abd08a3eb7b3e7ff32c783117aefa7397c0a046
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-13 21:07:57 +1000

    Create recognition model, can't run due to CUDA issues

--- keraspy/loader.py
+++ keraspy/loader.py
@@ -4,28 +4,26 @@ from pathlib import Path
 
 datadir = Path(__file__).resolve().parent.joinpath("dataset")
 
-gestures_count = 11
-images_count = 120
-
 
 def sync_shuffle(a, b):
     p = np.random.permutation(len(a))
     return np.array(a)[p], np.array(b)[p]
 
 
-def load_image(gesture, number):
+def load_image(gesture, number, height, width):
     path = str(datadir.joinpath(f"{gesture}/{number}.png"))
-    image = Image.open(path).resize((320, 240))
+    image = Image.open(path).resize((height, width))
     data = np.asarray(image) / 255
     return data, gesture - 1
 
 
-def load_dataset():
+def load_dataset(gestures_count, images_count, height, width):
     X, Y = [], []
 
     for i in range(1, gestures_count + 1):
+        print(f"{i}/{gestures_count + 1}")
         for j in range(1, images_count + 1):
-            x, y = load_image(i, j)
+            x, y = load_image(i, j, height, width)
             X.append(x)
             Y.append(y)
     X, Y = sync_shuffle(X, Y)
--- keraspy/main.py
+++ keraspy/main.py
@@ -1,5 +1,42 @@
+from keras import applications
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.models import Model
+import numpy as np
+
 from loader import load_dataset
 
-x_train, y_train, x_test, y_test = load_dataset()
 
-print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
+
+
+gestures_count = 11
+images_count = 12
+height = 224
+width = 224
+epochs = 10
+
+x_train, y_train, x_test, y_test = load_dataset(gestures_count, images_count, height, width)
+# print(x_train.shape, y_train.shape)
+
+y_train = convert_to_one_hot(y_train, gestures_count).T
+y_test = convert_to_one_hot(y_test, gestures_count).T
+
+base_model = applications.resnet50.ResNet50(weights=None, include_top=False, input_shape=(height, width, 3))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.7)(x)
+predictions = Dense(gestures_count, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+adam = Adam(lr=0.0001)
+model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=epochs, batch_size=64)
+
+preds = model.evaluate(x_test, y_test)
+print("Loss = " + str(preds[0]))
+print("Test Accuracy = " + str(preds[1]))
