ac2297359a5f164d9a6a5842f4de79cc4b1ee073
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-16 20:14:31 +1000

    Change pure learning to transfer learning

--- keraspy/main.py
+++ keraspy/main.py
@@ -1,10 +1,16 @@
+from pathlib import Path
+
 from keras import applications
-from keras.layers import Dense, Dropout, GlobalAveragePooling2D
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Flatten, BatchNormalization, Dropout
 from keras.optimizers import Adam
-from keras.models import Model
+from keras.models import Model, Sequential
 import numpy as np
+import tensorflow as tf
+import tensorflowjs as tfjs
 
 from loader import load_dataset
+
+modelpath = str(Path(__file__).resolve().parent.joinpath('model'))
 
 
 def convert_to_one_hot(Y, C):
@@ -16,7 +22,7 @@ gestures_count = 11
 images_count = 120
 height = 196
 width = 196
-epochs = 100
+epochs = 50
 
 x_train, y_train, x_test, y_test = load_dataset(gestures_count, images_count, height, width)
 # print(x_train.shape, y_train.shape)
@@ -24,21 +30,34 @@ x_train, y_train, x_test, y_test = load_dataset(gestures_count, images_count, he
 y_train = convert_to_one_hot(y_train, gestures_count).T
 y_test = convert_to_one_hot(y_test, gestures_count).T
 
-base_model = applications.resnet50.ResNet50(weights=None, include_top=False, input_shape=(height, width, 3))
+base_model = applications.resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=(height, width, 3))
 
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dropout(0.7)(x)
-predictions = Dense(gestures_count, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=predictions)
+for layer in base_model.layers[:143]:
+    layer.trainable = False
+
+model = Sequential()
+model.add(base_model)
+model.add(Flatten())
+model.add(BatchNormalization())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(gestures_count, activation='softmax'))
 
 adam = Adam(lr=0.0001)
 model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=epochs, batch_size=64)
+model.fit(x_train, y_train, epochs=epochs, batch_size=64, validation_data=(x_test, y_test))
 
 preds = model.evaluate(x_test, y_test)
 print("Loss = " + str(preds[0]))
 print("Test Accuracy = " + str(preds[1]))
 
 model.save('my_model.h5')
+tfjs.converters.save_keras_model(model, modelpath)
