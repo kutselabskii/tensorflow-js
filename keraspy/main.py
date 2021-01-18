b8dcb3dd9d7b1c3524169cc9ffbdcf12433b8399
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-18 21:26:09 +1000

    Add extended dataset (nothing changed)

--- keraspy/main.py
+++ keraspy/main.py
@@ -18,15 +18,33 @@ def convert_to_one_hot(Y, C):
     return Y
 
 
-gestures_count = 11
-images_count = 120
-height = 224
-width = 224
-epochs = 50
-batch_size = 32
-folder = 'dataset'
-format = '.png'
-save_path = 'my_model.h5'
+def set1():
+    global gestures_count, images_count, height, width, epochs, batch_size, folder, format, save_path
+    gestures_count = 11
+    images_count = 120
+    height = 224
+    width = 224
+    epochs = 50
+    batch_size = 32
+    folder = 'dataset'
+    format = '.png'
+    save_path = 'my_model.h5'
+
+
+def set2():
+    global gestures_count, images_count, height, width, epochs, batch_size, folder, format, save_path
+    gestures_count = 26
+    images_count = 160
+    height = 224
+    width = 224
+    epochs = 100
+    batch_size = 48
+    folder = 'dataset2'
+    format = '.jpg'
+    save_path = 'my_model2.h5'
+
+
+set2()
 
 x_train, y_train, x_test, y_test = load_dataset(gestures_count, images_count, height, width, folder, format)
 # print(x_train.shape, y_train.shape)
@@ -34,7 +52,7 @@ x_train, y_train, x_test, y_test = load_dataset(gestures_count, images_count, he
 y_train = convert_to_one_hot(y_train, gestures_count).T
 y_test = convert_to_one_hot(y_test, gestures_count).T
 
-base_model = applications.resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=(height, width, 3))
+base_model = applications.resnet50.ResNet50(weights="imagenet", include_top=False, input_shape=(height, width, 3))
 
 for layer in base_model.layers[:143]:
     layer.trainable = False
@@ -54,7 +72,7 @@ model.add(Dropout(0.5))
 model.add(BatchNormalization())
 model.add(Dense(gestures_count, activation='softmax'))
 
-adam = Adam(lr=0.0001)
+adam = Adam(lr=0.001)
 model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, y_test))
--- keraspy/preparer.py
+++ keraspy/preparer.py
@@ -12,15 +12,16 @@ for j in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
     cur = 1
     for i in range(1, person_count + 1):
         for k in range(1, pic_count + 1):
-            path = curdir.joinpath(f"dataset2/{ord(j) - ord('A') + 1}")
-            try:
-                os.makedirs(str(path))
-            except Exception:
-                pass
-            p_from = str(curdir.joinpath(f"gestures_dataset/HGM-4/HGM-1.0/Below_CAM/{j}/P{i}_00{k}{format}"))
-            p_to = str(path.joinpath(f"{cur}{format}"))
-            try:
-                shutil.copy(p_from, p_to)
-            except Exception:
-                continue
-            cur += 1
+            for z in ["Below", "Front", "Left", "Right"]:
+                path = curdir.joinpath(f"dataset2/{ord(j) - ord('A') + 1}")
+                try:
+                    os.makedirs(str(path))
+                except Exception:
+                    pass
+                p_from = str(curdir.joinpath(f"gestures_dataset/HGM-4/HGM-1.0/{z}_CAM/{j}/P{i}_00{k}{format}"))
+                p_to = str(path.joinpath(f"{cur}{format}"))
+                try:
+                    shutil.copy(p_from, p_to)
+                except Exception:
+                    continue
+                cur += 1
