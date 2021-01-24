4408cc1136b74fbcdccd0f45eccb90b1ca437a5d
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-24 15:14:14 +1000

    Fallback to no-model version

--- src/index.js
+++ src/index.js
@@ -1,88 +1,12 @@
 import * as tf from '@tensorflow/tfjs';
 import './css/style.css'
 
-import i1 from './img/1.png';
-import i2 from './img/2.png';
-import i3 from './img/3.png';
-import i4 from './img/4.png';
-import i5 from './img/5.png';
-import i6 from './img/6.png';
-import i7 from './img/7.png';
-import i8 from './img/8.png';
-import i9 from './img/9.png';
-import i10 from './img/10.png';
-import i11 from './img/11.png';
-
-const images = [
-  i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11
-]
-
 const enableWebcamButton = document.getElementById('webcamButton');
 const video = document.getElementById('webcam');
 const demosSection = document.getElementById('demos');
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
 const modelButton = document.getElementById('modelButton');
-const imgBlock = document.getElementById('images');
-var last = 0;
 var model = undefined;
-var imgNodes = [];
-
-for (let i = 0; i < 11; ++i) {
-  var im = document.createElement('img');
-  im.src = images[i];
-  im.classList.add('img');
-  imgBlock.appendChild(im);
-  imgNodes.push(im);
-}
-
-const camera = new Camera(video, {
-  onFrame: async () => {
-    await hands.send({image: video});
-  },
-  width: 1280,
-  height: 960
-});
-
-function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    let data = [];
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                      {color: '#00FF00', lineWidth: 5});
-      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
-      for (const i in landmarks) {
-        const mark = landmarks[i];
-        data.push([mark.x, mark.y]);
-      }
-    }
-
-    var predictions = model.predict(tf.tensor(data).reshape([1, 21, 2]));
-    var res = indexOfMax(predictions.arraySync()[0]);
-    if (res != last) {
-      imgNodes[last].classList.remove('chosen');
-      imgNodes[res].classList.add('chosen');
-    }
-    last = res;
-    // console.log(indexOfMax(predictions.arraySync()[0]));
-  }
-  canvasCtx.restore();
-}
-
-const hands = new Hands({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
-}});
-
-hands.setOptions({
-  maxNumHands: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
-hands.onResults(onResults);
+var webcam = undefined
 
 modelButton.addEventListener('click', modelButtonClicked);
 
@@ -96,9 +20,20 @@ enableWebcamButton.addEventListener('click', buttonClicked);
 
 function buttonClicked(event) {
   event.target.classList.add('removed');
-  // video.classList.add('removed');
-  camera.start();
   enableWebcamButton.setAttribute('display', 'none');
+
+  const constraints = {
+    video: true
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(async function(stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+
+    webcam = await tf.data.webcam(video, {
+      resizeWidth: 196,
+      resizeHeight: 196,
+    });
+  });
 }
 
 function indexOfMax(arr) {
@@ -115,3 +50,14 @@ function indexOfMax(arr) {
   }
   return maxIndex;
 }
+
+async function predictWebcam() {
+  // var img = await webcam.capture();
+  // img = img.reshape([1, 196, 196, 3]);
+
+  // var predictions = model.predict(img);
+  // console.log(indexOfMax(predictions));
+
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+} 
