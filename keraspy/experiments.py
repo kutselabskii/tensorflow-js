0fe2abbc356b0f70ab7787c478920859be2a08fb
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-27 21:14:34 +1000

    Add working texture merger

--- keraspy/experiments.py
+++ keraspy/experiments.py
@@ -11,16 +11,13 @@ savepath = Path(__file__).resolve().parent.joinpath('new.jpg')
 image = Image.open(path).convert('HSV')
 texture = Image.open(texpath).resize(image.size).convert('HSV')
 
-data = np.asarray(image.getdata())
-texdata = np.asarray(texture.getdata())
+data = image.load()
+texdata = texture.load()
 
-print(data.shape)
+for i in range(image.size[0]):
+    for j in range(image.size[1]):
+        xy = (i, j)
+        pixel = (texdata[i, j][0], texdata[i, j][1], data[i, j][2])
+        image.putpixel(xy, pixel)
 
-for i in range(len(data)):
-    data[i] = (texdata[i][0], texdata[i][1], data[i][2])
-
-repainted = Image.fromarray(data, 'HSV').convert('RGB').resize(image.size)
-# for i in range(100):
-#     print(repainted.getdata()[i])
-# quit()
-repainted.save(savepath)
+image.convert('RGB').save(savepath)
