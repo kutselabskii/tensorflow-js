408e0001cd0dea0f14e029c4ccd26650728a313d
Egor Kutselabskii <osutanfirst@gmail.com>
2021-02-08 20:32:50 +1000

    Add result plotting

--- keraspy/experiments.py
+++ keraspy/experiments.py
@@ -3,6 +3,9 @@ from PIL import Image
 from pathlib import Path
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 
 path = Path(__file__).resolve().parent.joinpath('sofa.jpg')
 texpath = Path(__file__).resolve().parent.joinpath('texture.jpg')
@@ -21,3 +24,19 @@ for i in range(image.size[0]):
         image.putpixel(xy, pixel)
 
 image.convert('RGB').save(savepath)
+
+original_image = mpimg.imread(path)
+texture_image = mpimg.imread(texpath)
+resulting_image = mpimg.imread(savepath)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 3, 1)
+imgplot = plt.imshow(original_image)
+ax.set_title('Original')
+ax = fig.add_subplot(1, 3, 2)
+imgplot = plt.imshow(texture_image)
+ax.set_title('Texture')
+ax = fig.add_subplot(1, 3, 3)
+imgplot = plt.imshow(resulting_image)
+ax.set_title('Result')
+plt.show()
