df9c92fe8dc4925c2d00ceef33f271d7b8dbf654
Egor Kutselabskii <osutanfirst@gmail.com>
2021-02-19 20:48:53 +1000

    Update dataset loader for new dataset

--- keraspy/loader.py
+++ keraspy/loader.py
@@ -1,14 +1,9 @@
 import numpy as np
 from PIL import Image
 from pathlib import Path
-import mediapipe as mp
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=1,
-    min_detection_confidence=0.5
-)
+
+path = "D:/Git/NDDS Generated/TestCapturer"
 
 
 def sync_shuffle(a, b):
@@ -16,41 +11,24 @@ def sync_shuffle(a, b):
     return np.array(a)[p], np.array(b)[p]
 
 
-def load_image(gesture, number, height, width, datadir, format):
-    path = str(datadir.joinpath(f"{gesture}/{number}{format}"))
-    image = Image.open(path).resize((height, width))
-    data = np.asarray(image) / 255
-    return data, gesture - 1
-
-
-def load_hand_image(gesture, number, height, width, datadir, format):
-    path = str(datadir.joinpath(f"{gesture}/{number}{format}"))
+def load_image(number, height, width, subtype, datadir):
+    strnumber = '0' * (6 - len(str(number))) + str(number)
+    path = str(datadir.joinpath(f"{strnumber}{subtype}.png"))
     image = Image.open(path).resize((height, width))
     data = np.asarray(image)
-    hd = hands.process(data)
-    result = None
-    if hd is not None:
-        marks = hd.multi_hand_landmarks[0]
-        result = np.array([[mark.x, mark.y] for mark in marks.landmark])
-    return result, gesture - 1
+    return data
 
 
-def load_dataset(gestures_count, images_count, height, width, folder, format):
+def load_dataset(count, height, width):
     X, Y = [], []
-    datadir = Path(__file__).resolve().parent.joinpath(folder)
+    datadir = Path(path)
 
-    for i in range(1, gestures_count + 1):
-        print(f"{i}/{gestures_count + 1}")
-        for j in range(1, images_count + 1):
-            try:
-                x, y = load_hand_image(i, j, height, width, datadir, format)
-                if x is None:
-                    continue
-                X.append(x)
-                Y.append(y)
-            except Exception:
-                print(f'Skipped {i}/{j}')
-                continue
+    for i in range(count):
+        print(f"{i}/{count}")
+        x = load_image(i, height, width, '', datadir)
+        y = load_image(i, height, width, '.cs', datadir)
+        X.append(x)
+        Y.append(y)
     X, Y = sync_shuffle(X, Y)
 
     divider = round(len(X) / 100 * 90)
