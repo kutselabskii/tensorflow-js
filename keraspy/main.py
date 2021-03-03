f46d8234b91219f2479fc1b343fe74fa21239e38
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-03 22:25:13 +1000

    Add model checkpoint saving

--- keraspy/main.py
+++ keraspy/main.py
@@ -2,12 +2,14 @@ from pathlib import Path
 
 import segmentation_models as sm
 import tensorflowjs as tfjs
+import tensorflow as tf
 
 from loader import CustomDataset
 
 sm.set_framework('tf.keras')
 modelpath = str(Path(__file__).resolve().parent.joinpath('model'))
 save_path = 'segmentation_model.h5'
+checkpoint_path = 'checkpoint_model.h5'
 
 BACKBONE = 'resnet34'
 CLASSES = ['sofa']
@@ -30,13 +32,20 @@ total_loss = sm.losses.binary_focal_dice_loss
 metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
 model.compile("ADAM", total_loss, metrics)
 
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    monitor='val_iou_score',
+    mode='max',
+    save_best_only=True)
+
 model.fit_generator(
    generator=train_generator,
    validation_data=val_generator,
    # use_multiprocessing=True,
    use_multiprocessing=False,
    workers=2,
-   epochs=EPOCHS
+   epochs=EPOCHS,
+   callbacks=[model_checkpoint_callback]
 )
 
 model.save(save_path)
