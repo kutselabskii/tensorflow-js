d4e2d9a3a8c1495ed659d8f46f4c8c9851ffb229
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-22 20:21:15 +1000

    Add working checker (network failed)

--- keraspy/checker.py
+++ keraspy/checker.py
@@ -1,16 +1,13 @@
-import segmentation_models as sm
-import tensorflowjs as tfjs
 import tensorflow as tf
-
+from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
 
 from loader import CustomDataset
 
-sm.set_framework('tf.keras')
-
 model = tf.keras.models.load_model('segmentation_model.h5', compile=False)
 testing = CustomDataset(batch_size=1, count=100)
 
-res = model.predict(testing[0][0])
-plt.plot(res[0])
-plt.show()
+res = model.predict(testing[0][0])[0]
+img = Image.fromarray(np.squeeze((res * 255).astype(np.uint8), axis=2))
+img.show()
