a6fbbf3f194cf84f6925f7197ec8fc0cac620d0a
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-31 22:57:21 +1000

    Add checker for real sofa images

--- keraspy/checker.py
+++ keraspy/checker.py
@@ -5,9 +5,24 @@ import matplotlib.pyplot as plt
 
 from loader import CustomDataset
 
-model = tf.keras.models.load_model('segmentation_model.h5', compile=False)
-testing = CustomDataset(batch_size=1, count=100)
+check_real = True
+use_checkpoint = True
+real_number = 7
 
-res = model.predict(testing[0][0])[0]
+if use_checkpoint:
+    modelpath = 'checkpoint_model.h5'
+else:
+    modelpath = 'segmentation_model.h5'
+
+model = tf.keras.models.load_model(modelpath, compile=False)
+
+if check_real:
+    path = f"D:/Git/tensorflow-js/keraspy/sofa/0000000{real_number}.jpg"
+    image = Image.open(path).convert('RGB').resize((480, 480))
+    data = np.array([np.asarray(image).astype('float32')])
+else:
+    data = CustomDataset(batch_size=1, count=100)[5][0]
+
+res = model.predict(data)[0]
 img = Image.fromarray(np.squeeze((res * 255).astype(np.uint8), axis=2))
 img.show()
