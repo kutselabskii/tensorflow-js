6c5d5bd862a58bda74d58b30e6de6043fdf0a12b
Egor Kutselabskii <osutanfirst@gmail.com>
2021-04-06 22:28:11 +1000

    Change threshold to 0.75 (became even worse)

--- keraspy/main.py
+++ keraspy/main.py
@@ -34,7 +34,7 @@ model = sm.Unet(
 )
 
 total_loss = sm.losses.binary_focal_dice_loss
-metrics = [sm.metrics.IOUScore(), sm.metrics.FScore()]
+metrics = [sm.metrics.IOUScore(threshold=0.75), sm.metrics.FScore(threshold=0.75)]
 model.compile("ADAM", total_loss, metrics)
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
