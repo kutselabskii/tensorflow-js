5083b9e8cd389bfb51d2b8916abf87433e231c22
Egor Kutselabskii <osutanfirst@gmail.com>
2021-06-11 21:40:40 +1000

    Use new Fast SCNN architecture (fails to run for now)

--- keraspy/main.py
+++ keraspy/main.py
@@ -1,98 +1,25 @@
 from pathlib import Path
 
-#import segmentation_models as sm
 import tensorflowjs as tfjs
 import tensorflow as tf
+from tensorflow import keras
 
 from loader import CustomDataset
-
-from tensorflow import keras
-# from tensorflow.keras import layers
-
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dropout, Lambda
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import concatenate
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras import backend as K
+import models
 
 
-def get_model(img_size):
-    inputs = Input((img_size[0], img_size[1], 3))
-    # s = Lambda(lambda x: x / 255) (inputs)
-
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (inputs)
-    c1 = Dropout(0.1) (c1)
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
-    p1 = MaxPooling2D((2, 2)) (c1)
-
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
-    c2 = Dropout(0.1) (c2)
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
-    p2 = MaxPooling2D((2, 2)) (c2)
-
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
-    c3 = Dropout(0.2) (c3)
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
-    p3 = MaxPooling2D((2, 2)) (c3)
-
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
-    c4 = Dropout(0.2) (c4)
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
-    c5 = Dropout(0.3) (c5)
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
-
-    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
-    u6 = concatenate([u6, c4])
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
-    c6 = Dropout(0.2) (c6)
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
-
-    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
-    u7 = concatenate([u7, c3])
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
-    c7 = Dropout(0.2) (c7)
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
-
-    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
-    u8 = concatenate([u8, c2])
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
-    c8 = Dropout(0.1) (c8)
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
-
-    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
-    u9 = concatenate([u9, c1], axis=3)
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    c9 = Dropout(0.1) (c9)
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
-
-    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
-
-    model = Model(inputs=[inputs], outputs=[outputs])
-    return model
-    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
-    # model.summary()
-
-# sm.set_framework('tf.keras')
 modelpath = str(Path(__file__).resolve().parent.joinpath('model'))
-save_path = 'segmentation_model_mine.h5'
-checkpoint_path = 'checkpoint_model_mine.h5'
+save_path = 'fast_scnn.h5'
+checkpoint_path = 'checkpoint_fast_scnn.h5'
 
 BACKBONE = 'resnet34'
 CLASSES = ['sofa']
 LR = 0.001
 EPOCHS = 100
-BATCH_SIZE = 16
+BATCH_SIZE = 4
 IMG_COUNT = 900
-TRAIN_PERCENTAGE = 0.9
-IMG_SIZE = (224, 224)
-
-# preprocess_input = sm.get_preprocessing(BACKBONE)
+TRAIN_PERCENTAGE = 0.92
+IMG_SIZE = (2048, 1024)
 
 train_amount = round(IMG_COUNT * TRAIN_PERCENTAGE)
 val_amount = round(IMG_COUNT * (1 - TRAIN_PERCENTAGE))
@@ -100,20 +27,8 @@ val_amount = round(IMG_COUNT * (1 - TRAIN_PERCENTAGE))
 train_generator = CustomDataset(batch_size=BATCH_SIZE, count=train_amount, img_size=IMG_SIZE)
 val_generator = CustomDataset(batch_size=BATCH_SIZE, count=val_amount, img_size=IMG_SIZE, offset=train_amount)
 
-# model = sm.Linknet(
-#     BACKBONE,
-#     classes=len(CLASSES),
-#     encoder_weights=None
-# )
-
 keras.backend.clear_session()
-model = get_model(IMG_SIZE)
-
-#total_loss = sm.losses.binary_focal_dice_loss
-#metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
-
-metrics = [tf.keras.metrics.MeanIoU(num_classes=2)]
-model.compile("adam", "sparse_categorical_crossentropy", metrics)
+model = models.get_model(IMG_SIZE)
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_path,
@@ -121,22 +36,11 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     mode='max',
     save_best_only=True)
 
-# model.fit_generator(
-#    generator=train_generator,
-#    validation_data=val_generator,
-#    # use_multiprocessing=True,
-#    use_multiprocessing=False,
-#    #workers=2,
-#    epochs=EPOCHS,
-#    callbacks=[model_checkpoint_callback]
-# )
+print(model.summary())
 
 model.fit(
    train_generator,
    validation_data=val_generator,
-   # use_multiprocessing=True,
-   #use_multiprocessing=False,
-   #workers=2,
    epochs=EPOCHS,
    callbacks=[model_checkpoint_callback]
 )
